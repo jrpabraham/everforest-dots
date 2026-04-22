1ff0f25fd35d37145bea282504348ed48e5d621f
Justin <jrpabraham@gmail.com>
2026-04-23 05:50:46 +0900

    Lock and idle manager.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,3 +14,17 @@ end
 #function fish_greeting
 #    # smth smth
 #end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/justin/miniconda3/bin/conda
+    eval /home/justin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/justin/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/justin/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/justin/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
